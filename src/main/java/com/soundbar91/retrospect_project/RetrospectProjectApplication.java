809fe90689cb9ad09061f800f8165ea1ef3b9065
신관규 <soundbar91@gmail.com>
2024-07-14 14:38:27 +0900

    Initial commit

---
+++ src/main/java/com/soundbar91/retrospect_project/RetrospectProjectApplication.java
@@ -0,0 +1,13 @@
+package com.soundbar91.retrospect_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrospectProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrospectProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soundbar91/retrospect_project/RetrospectProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundbar91.retrospect_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrospectProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
