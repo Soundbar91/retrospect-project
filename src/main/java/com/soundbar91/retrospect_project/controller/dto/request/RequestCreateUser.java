b20697dc3b492044b73a581c15c85b910544c7f6
신관규 <soundbar91@gmail.com>
2024-07-18 15:17:58 +0900

    feat: 회원가입 유효성 검사 추가

--- src/main/java/com/soundbar91/retrospect_project/controller/dto/request/RequestCreateUser.java
+++ src/main/java/com/soundbar91/retrospect_project/controller/dto/request/RequestCreateUser.java
@@ -1,9 +1,20 @@
 package com.soundbar91.retrospect_project.controller.dto.request;
 
 import com.soundbar91.retrospect_project.entity.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record RequestCreateUser(
+        @NotBlank(message = "이메일은 필수 입력 값입니다.")
+        @Pattern(message = "이메일은 user@example.com와 같은 형식으로 입력해야합니다.",
+                regexp = "^[a-zA-Z0-9]+@[0-9a-zA-Z]+\\.[a-z]+$")
         String email,
+
+        @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+        @Size(message = "비밀번호는 최소 8자리, 최대 20자리입니다.", min = 8, max = 20)
+        @Pattern(message = "비밀번호는 영어, 숫자 그리고 특수문자로 구성되며, !@#$%^&*()/ 중 하나 이상을 포함해야 합니다.",
+                regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.+[!@#$%^&*()/]).{8,20}$")
         String password
 ) {
     public User toEntity(String password) {
