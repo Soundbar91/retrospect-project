d2347ac12c6fa851662379bbf716ede6c81db0f8
신관규 <soundbar91@gmail.com>
2024-07-21 20:38:58 +0900

    refactor: 세션 예외 추가

--- src/main/java/com/soundbar91/retrospect_project/interceptor/CertInterceptor.java
+++ src/main/java/com/soundbar91/retrospect_project/interceptor/CertInterceptor.java
@@ -1,16 +1,20 @@
 package com.soundbar91.retrospect_project.interceptor;
 
+import com.soundbar91.retrospect_project.exception.ApplicationException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import static com.soundbar91.retrospect_project.exception.errorCode.UserErrorCode.NOT_LOGIN;
 
 @Component
 public class CertInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return request.getSession().getAttribute("userId") != null;
+        if (request.getSession().getAttribute("userId") == null) throw new ApplicationException(NOT_LOGIN);
+        else return true;
     }
 
 }
