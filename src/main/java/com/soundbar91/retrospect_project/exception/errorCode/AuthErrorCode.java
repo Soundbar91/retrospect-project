a4ad5b9074826299fd47a36667a2f648357bbec4
신관규 <soundbar91@gmail.com>
2024-08-05 18:10:32 +0900

    feat: Auth API 추가

---
+++ src/main/java/com/soundbar91/retrospect_project/exception/errorCode/AuthErrorCode.java
@@ -0,0 +1,18 @@
+package com.soundbar91.retrospect_project.exception.errorCode;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode implements ErrorCode {
+
+    DUPLICATE_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입된 이메일입니다"),
+    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "이미 가입된 아이디입니다"),
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
---
+++ src/main/java/com/soundbar91/retrospect_project/service/AuthService.java
@@ -0,0 +1,55 @@
+package com.soundbar91.retrospect_project.service;
+
+import com.soundbar91.retrospect_project.controller.dto.request.RequestLoginUser;
+import com.soundbar91.retrospect_project.entity.User;
+import com.soundbar91.retrospect_project.exception.ApplicationException;
+import com.soundbar91.retrospect_project.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.soundbar91.retrospect_project.exception.errorCode.AuthErrorCode.NOT_MATCH_PASSWORD;
+import static com.soundbar91.retrospect_project.exception.errorCode.UserErrorCode.NOT_FOUND_USER;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void loginUser(RequestLoginUser requestLoginUser, HttpServletRequest httpServletRequest) {
+        User user = authenticateUser(requestLoginUser);
+        authorizeSession(httpServletRequest, user);
+    }
+
+    @Transactional
+    public void logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        session.invalidate();
+    }
+
+    private User authenticateUser(RequestLoginUser requestLoginUser) {
+        User user = userRepository.findByUsername(requestLoginUser.username())
+                .orElseThrow(() -> new ApplicationException(NOT_FOUND_USER));
+
+        if (!passwordEncoder.matches(requestLoginUser.password(), user.getPassword()))
+            throw new ApplicationException(NOT_MATCH_PASSWORD);
+
+        return user;
+    }
+
+    private void authorizeSession(HttpServletRequest request, User user) {
+        request.getSession().invalidate();
+        HttpSession session = request.getSession(true);
+
+        session.setAttribute("userId", user.getId());
+        session.setAttribute("username", user.getUsername());
+        session.setMaxInactiveInterval(3600);
+    }
+
+}
