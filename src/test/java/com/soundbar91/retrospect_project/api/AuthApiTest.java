f5e931d41a12ec883deeebda66580b3e3fd57ddd
신관규 <soundbar91@gmail.com>
2024-09-06 13:49:12 +0900

    refactor: 유저 로그인 테스트 코드

---
+++ src/test/java/com/soundbar91/retrospect_project/api/AuthApiTest.java
@@ -0,0 +1,52 @@
+package com.soundbar91.retrospect_project.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.soundbar91.retrospect_project.fixture.UserFixture;
+import com.soundbar91.retrospect_project.user.entity.User;
+
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@SuppressWarnings("NonAsciiCharacters")
+@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AuthApiTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserFixture userFixture;
+
+    @Test
+    void 회원_로그인() throws Exception {
+        User user = userFixture.사용자();
+        String username = user.getUsername();
+        String password = "testPassword";
+
+        mockMvc.perform(
+                post("/auth/login")
+                    .content("""
+                        {
+                          "username" : "%s",
+                          "password" : "%s"
+                        }
+                        """.formatted(username, password))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().is3xxRedirection())
+            .andExpect(request().sessionAttribute("username", username));
+    }
+}
---
+++ src/test/java/com/soundbar91/retrospect_project/fixture/UserFixture.java
@@ -0,0 +1,31 @@
+package com.soundbar91.retrospect_project.fixture;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.soundbar91.retrospect_project.user.entity.User;
+import com.soundbar91.retrospect_project.user.repository.UserRepository;
+
+@Component
+@SuppressWarnings("NonAsciiCharacters")
+public class UserFixture {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public UserFixture(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+    }
+
+    public User 사용자() {
+        return userRepository.save(
+            User.builder()
+                .username("testUser")
+                .email("testUser@gmail.com")
+                .password(passwordEncoder.encode("testPassword"))
+                .isActive(true)
+                .build()
+        );
+    }
+}
