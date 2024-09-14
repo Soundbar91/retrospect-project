b6f2ad09d86301e5769b7f85a07b739692c101e2
신관규 <soundbar91@gmail.com>
2024-09-14 13:27:04 +0900

    refactor: 뭔가 변경을 했다.

--- src/main/java/com/soundbar91/retrospect_project/global/config/WebConfig.java
+++ src/main/java/com/soundbar91/retrospect_project/global/config/WebConfig.java
@@ -1,13 +1,18 @@
 package com.soundbar91.retrospect_project.global.config;
 
-import java.util.List;
-
-import com.soundbar91.retrospect_project.global.interceptor.*;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.soundbar91.retrospect_project.global.interceptor.AuthInterceptor;
+import com.soundbar91.retrospect_project.global.interceptor.CommentInterceptor;
+import com.soundbar91.retrospect_project.global.interceptor.PostInterceptor;
+import com.soundbar91.retrospect_project.global.interceptor.ProblemInterceptor;
+import com.soundbar91.retrospect_project.global.interceptor.ResultInterceptor;
+import com.soundbar91.retrospect_project.global.interceptor.UserInterceptor;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,28 +29,28 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(certInterceptor)
-                .order(1)
-                .addPathPatterns("/auth/logout");
+            .order(1)
+            .addPathPatterns("/auth/logout");
 
         registry.addInterceptor(userInterceptor)
-                .order(1)
-                .addPathPatterns("/user/**");
+            .order(1)
+            .addPathPatterns("/user/**");
 
         registry.addInterceptor(problemInterceptor)
-                .order(1)
-                .addPathPatterns("/problem/**", "/problems");
+            .order(1)
+            .addPathPatterns("/problem/**", "/problems");
 
         registry.addInterceptor(resultInterceptor)
-                .order(1)
-                .addPathPatterns("/result/**", "/results");
+            .order(1)
+            .addPathPatterns("/result/**", "/results");
 
         registry.addInterceptor(boardInterceptor)
-                .order(1)
-                .addPathPatterns("/post/**", "/posts");
+            .order(1)
+            .addPathPatterns("/post/**", "/posts");
 
         registry.addInterceptor(commentInterceptor)
-                .order(1)
-                .addPathPatterns("/comment/**");
+            .order(1)
+            .addPathPatterns("/comment/**");
 
     }
 
@@ -58,5 +63,4 @@ public class WebConfig implements WebMvcConfigurer {
             .allowCredentials(true)
             .maxAge(3600);
     }
-
 }
