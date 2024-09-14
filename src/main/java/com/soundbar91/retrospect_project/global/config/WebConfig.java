06170e305ecf6e315db55c10dc2bcf176adb29a3
신관규 <soundbar91@gmail.com>
2024-09-14 20:29:43 +0900

    refactor: cors 설정 삭제

--- src/main/java/com/soundbar91/retrospect_project/global/config/WebConfig.java
+++ src/main/java/com/soundbar91/retrospect_project/global/config/WebConfig.java
@@ -49,14 +49,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
-            .allowedHeaders("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(3600);
-    }
+    // @Override
+    // public void addCorsMappings(CorsRegistry registry) {
+    //     registry.addMapping("/**")
+    //         .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
+    //         .allowedHeaders("GET", "POST", "PUT", "DELETE")
+    //         .allowedHeaders("*")
+    //         .allowCredentials(true)
+    //         .maxAge(3600);
+    // }
 
 }
