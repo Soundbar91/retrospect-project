a87ea2f705717d0127c1e14bc9c479fc9d67c081
신관규 <soundbar91@gmail.com>
2024-09-16 16:27:13 +0900

    임시 업데이트 (이후에 삭제 필요) (#57)

--- src/main/java/com/soundbar91/retrospect_project/result/controller/ResultController.java
+++ src/main/java/com/soundbar91/retrospect_project/result/controller/ResultController.java
@@ -5,9 +5,11 @@ import com.soundbar91.retrospect_project.result.controller.dto.ResponseResult;
 import com.soundbar91.retrospect_project.result.entity.keyInstance.Grade;
 import com.soundbar91.retrospect_project.result.entity.keyInstance.Language;
 import com.soundbar91.retrospect_project.result.service.ResultService;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,9 +23,9 @@ public class ResultController implements ResultApi {
 
     @PostMapping("/problem/{problemId}/result")
     public ResponseEntity<ResponseResult> createResult(
-            @Valid @RequestBody RequestSubmit requestCreateResult,
-            @PathVariable(value = "problemId") Long problemId,
-            HttpServletRequest httpServletRequest
+        @Valid @RequestBody RequestSubmit requestCreateResult,
+        @PathVariable(value = "problemId") Long problemId,
+        HttpServletRequest httpServletRequest
     ) {
         ResponseResult result = resultService.createResult(requestCreateResult, httpServletRequest, problemId);
         return ResponseEntity.ok(result);
@@ -31,18 +33,19 @@ public class ResultController implements ResultApi {
 
     @GetMapping("/result/{resultId}")
     public ResponseEntity<ResponseResult> getResult(
-            @PathVariable(value = "resultId") Long resultId
+        @PathVariable(value = "resultId") Long resultId,
+        HttpServletRequest httpServletRequest
     ) {
-        ResponseResult result = resultService.getResult(resultId);
+        ResponseResult result = resultService.getResult(resultId, httpServletRequest);
         return ResponseEntity.ok(result);
     }
 
     @GetMapping("/results")
     public ResponseEntity<List<ResponseResult>> getResults(
-            @RequestParam(value = "grade", required = false) Grade grade,
-            @RequestParam(value = "language", required = false) Language language,
-            @RequestParam(value = "username", required = false) String username,
-            @RequestParam(value = "problemId", required = false) Long problemId
+        @RequestParam(value = "grade", required = false) Grade grade,
+        @RequestParam(value = "language", required = false) Language language,
+        @RequestParam(value = "username", required = false) String username,
+        @RequestParam(value = "problemId", required = false) Long problemId
     ) {
         List<ResponseResult> results = resultService.getResults(grade, language, username, problemId);
         return ResponseEntity.ok(results);
